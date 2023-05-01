4bd1226c5fb58f552ed327672c22199194c2257d
Humanwine1 <106860142+Humanwine1@users.noreply.github.com>
2023-05-01 16:05:46 -0700

    added employee data script

---
+++ insert_employee_data.sql
@@ -0,0 +1,9 @@
+USE littlelemondb;
+
+INSERT INTO Employees (EmployeeID, Name, Role, ContactNumber, Email, AnnualSalary) VALUES
+(01,'Mario Gollini','Manager',351258074,'Mario.g@littlelemon.com','$70,000'),
+(02,'Adrian Gollini','Assistant Manager',351474048,'Adrian.g@littlelemon.com','$65,000'),
+(03,'Giorgos Dioudis','Head Chef',351970582,'Giorgos.d@littlelemon.com','$50,000'),
+(04,'Fatma Kaya','Assistant Chef',351963569,'Fatma.k@littlelemon.com','$45,000'),
+(05,'Elena Salvai','Head Waiter',351074198,'Elena.s@littlelemon.com','$40,000'),
+(06,'John Millar','Receptionist',351584508,'John.m@littlelemon.com','$35,000');
