eb0379e84d43d92d323d6ef96287556f55c1cd3f
Humanwine1 <106860142+Humanwine1@users.noreply.github.com>
2023-05-01 16:04:50 -0700

    added insert orders script

---
+++ insert_Orders_data.sql
@@ -0,0 +1,10 @@
+use littlelemondb;
+
+SET FOREIGN_KEY_CHECKS = 0;
+INSERT INTO Orders (OrderID, TableNo, MenuID, BookingID, Quantity, TotalCost)
+VALUES
+(1, 12, 1, 1, 2, 86),
+(2, 19, 2, 2, 1, 37),
+(3, 15, 2, 3, 1, 37),
+(4, 5, 3, 4, 1, 40),
+(5, 8, 1, 5, 1, 43);
