eea32c9917c8d7f5ce825e7d501acaaed02f4926
Humanwine1 <106860142+Humanwine1@users.noreply.github.com>
2023-05-01 16:04:19 -0700

    adding customer details script

---
+++ insert_CustomerDetails_data.sql
@@ -0,0 +1,10 @@
+use littlelemondb;
+
+INSERT INTO customerdetails (CustomerID, GuestFirstName, GuestLastName, Email, PhoneNumber)
+VALUES
+(1,'Anna','Iversen','annaiversen@gmail.com', 654654654),
+(2,'Joakim', 'Iversen', 'joakimiversen@gmail.com', 235624523),
+(3,'Vanessa', 'McCarthy', 'vmccarthy@yahoo.com', 234232344),
+(4,'Marcos', 'Romero', 'marromero@gmail.com', 534234234),
+(5,'Hiroki', 'Yamane', 'HirokiYamane@gmail.com', 444332233),
+(6,'Diana', 'Pinto', 'DPinto@gmail.com', 654455364);
